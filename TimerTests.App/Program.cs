66eee534fd118695f05405164b76d5ddfd13b019
Anton <khdevnet@gmail.con>
2024-04-21 22:13:44 +0200

    add timer tests

---
+++ TimerTests.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Timers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var timer = new System.Timers.Timer(1000);
+        timer.Elapsed += (object sender, ElapsedEventArgs e) => Console.WriteLine("Elapsed");
+        timer.AutoReset = false;
+        timer.Start();
+
+        Console.WriteLine("Start sleep");
+        Thread.Sleep(2000);
+        Console.WriteLine("End sleep");
+        timer.Start();
+
+        Console.ReadLine();
+    }
+}
